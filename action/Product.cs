112b2d9fd7c3afb7b88c0a5bb89db3467d7bf0c7
Arthur Mendes Gomes da Silva <arthur.silva@neovero.com>
2024-02-16 09:16:58 -0300

    action e Func

---
+++ action/Product.cs
@@ -0,0 +1,17 @@
+namespace action;
+
+public class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+    public Product(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+    public override string ToString()
+    {
+        return $"Name: {Name} ,  Preco:{Price}";
+    }
+}
---
+++ action/Program.cs
@@ -0,0 +1,23 @@
+﻿using action;
+
+List<Product> list = new List<Product>();
+list.Add(new Product("Tv", 900.00));
+list.Add(new Product("Mouse", 50.00));
+list.Add(new Product("Tablet", 350.50));
+list.Add(new Product("HD Case", 80.90));
+
+
+// Action<Product> act = p => {p.Price = p.Price * 1,10;};
+
+list.ForEach(x => {x.Price = x.Price * 1.10;});
+
+foreach(var item in list)
+{
+    Console.WriteLine(item.Price);
+}
+
+
+Func<Product, string> func = x => x.Name.ToUpper();
+
+var novalista = list.Select(func).ToList();
+var novalista1 = list.Select(x => x.Name.ToUpper());
